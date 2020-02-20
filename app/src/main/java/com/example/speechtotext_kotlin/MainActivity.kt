31fb82dc0ac3c78ae980d19c12d6ab3f13e4b8df
thehatch4815162342 <sdubois@kent.edu>
2020-02-20 15:33:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/speechtotext_kotlin/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.speechtotext_kotlin
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_SPEECH_INPUT = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //button click to show SpeechToText dialog
+        voiceBtn.setOnClickListener {
+            speak ();
+        }
+    }
+
+    private fun speak() {
+        //intent to show SpeechToText dialog
+        val mIntent = Intent (RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi speak something")
+
+        try{
+            //if there is no error show SpeechToText dialog
+            startActivityForResult(mIntent, REQUEST_CODE_SPEECH_INPUT)
+        }
+        catch (e:Exception){
+            //if there is any error get error message and show in toast
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+    //receive vocie input
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT -> {
+                if (requestCode == Activity.RESULT_OK && null != data) {
+                    //get text from result
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    //set the text to textview
+                    textTv.text = result[0]
+                }
+            }
+        }
+    }
+}
